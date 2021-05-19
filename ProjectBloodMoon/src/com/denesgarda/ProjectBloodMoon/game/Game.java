732bc363088e820faa6248ba6b37d4001318fa08
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-05-19 11:34:07 -0700

    Created login

--- ProjectBloodMoon/src/com/denesgarda/ProjectBloodMoon/game/Game.java
+++ ProjectBloodMoon/src/com/denesgarda/ProjectBloodMoon/game/Game.java
@@ -3,30 +3,84 @@ package com.denesgarda.ProjectBloodMoon.game;
 import com.denesgarda.ProjectBloodMoon.Main;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Game {
-    public static void game() throws IOException {
+    public static String username;
+
+    public static void game() throws IOException, SQLException {
         mainMenuLoop:
         while(true) {
             System.out.println("\nWelcome to Project: Blood Moon\n==============================\n1) Play\n2) Quit\nps. Type \"exit\" at any time to save and exit.");
             String mainMenuInput = Main.consoleInput.readLine();
-            if (mainMenuInput.equals("1")) {
-                System.out.println("login/signup");
-            }
-            else if(mainMenuInput.equals("2")) {
+            if (mainMenuInput.equalsIgnoreCase("1")) {
+                loginSignup:
+                while(true) {
+                    System.out.println("\nLogin / Signup\n1) Login\n2) Signup");
+                    String loginSignupInput = Main.consoleInput.readLine();
+                    if(loginSignupInput.equalsIgnoreCase("1")) {
+                        username = login();
+                        break;
+                    }
+                    else if(loginSignupInput.equalsIgnoreCase("2")) {
+                        System.out.println("This is not yet available");
+                    }
+                    else if(loginSignupInput.equalsIgnoreCase("exit")) {
+                        exit();
+                    }
+                    else invalid();
+                }
+                System.out.println("GAME");
                 exit();
             }
-            else if(mainMenuInput.equals("exit")) {
+            else if(mainMenuInput.equalsIgnoreCase("2")) {
                 exit();
             }
-            else {
-                System.out.println("That is not a valid input! Please try again.");
+            else if(mainMenuInput.equalsIgnoreCase("exit")) {
+                exit();
             }
+            else invalid();
         }
+    }
+
+    public static void invalid() {
+        System.out.println("That is not a valid input! Please try again.");
     }
 
     public static void exit() {
         System.out.println("Thank you for playing! Bye...");
         System.exit(0);
+    }
+    public static void saveAndExit() {
+        //save
+        System.out.println("Thank you for playing! Bye...");
+        System.exit(0);
+    }
+
+    public static String login() throws IOException, SQLException {
+        while(true) {
+            System.out.print("Username: ");
+            String username = Main.consoleInput.readLine();
+            System.out.print("Password: ");
+            String password = Main.consoleInput.readLine();
+
+            Statement stmt = Main.conn.createStatement();
+            String query = "SELECT password FROM pbm.accounts WHERE username = \"" + username + "\"";
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs.next()) {
+                if(rs.getString("password").equals(password)) {
+                    System.out.println("Logging in...");
+                    return username;
+                }
+                else {
+                    System.out.println("Wrong password! Please try again.");
+                }
+            }
+            else {
+                System.out.println("Username not found! Please try again.");
+            }
+        }
     }
 }
