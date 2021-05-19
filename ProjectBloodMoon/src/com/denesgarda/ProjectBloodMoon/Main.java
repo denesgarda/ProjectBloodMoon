df44e35553b09b83f3554a4ab02d3b4d2264c8bc
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-05-19 10:29:48 -0700

    Established connection

--- ProjectBloodMoon/src/com/denesgarda/ProjectBloodMoon/Main.java
+++ ProjectBloodMoon/src/com/denesgarda/ProjectBloodMoon/Main.java
@@ -1,7 +1,16 @@
 package com.denesgarda.ProjectBloodMoon;
 
+import com.denesgarda.ProjectBloodMoon.connection.Connect;
+
+import java.util.logging.Logger;
+
 public class Main {
+    public static java.sql.Connection conn = null;
+    public static Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        
+        logger.info("Project: Blood Moon, by Denes G. and Henry K., beta0.1");
+        conn = Connect.connect(conn);
+
     }
 }
