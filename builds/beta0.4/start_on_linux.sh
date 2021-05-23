7e80b75e4942dba832b9dac10f2e48d64b53f093
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-05-22 17:51:03 -0700

    Made .command and .sh files pause before exitting

--- builds/beta0.4/start_on_linux.sh
+++ builds/beta0.4/start_on_linux.sh
@@ -2,3 +2,5 @@
 
 cd "${0%/*}"
 java -jar ProjectBloodMoon.jar || echo Components to run the game are missing. Please download them at https://drive.google.com/drive/folders/1_aDBmAlZtB544GnEiuPZ-ENGvlCl6Lm8?usp=sharing
+echo Press any key to continue...
+read –n1
