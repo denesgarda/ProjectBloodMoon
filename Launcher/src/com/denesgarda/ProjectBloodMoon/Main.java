c9aa857e641e405a076c19fe91dc8c86d4e0f0c5
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-05-23 10:14:12 -0700

    u

--- Launcher/src/com/denesgarda/ProjectBloodMoon/Main.java
+++ Launcher/src/com/denesgarda/ProjectBloodMoon/Main.java
@@ -1,17 +1,17 @@
 package com.denesgarda.ProjectBloodMoon;
 
 import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.sql.*;
 import java.util.Properties;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Logger;
 
-import static com.denesgarda.ProjectBloodMoon.game.Game.exit;
-import static com.denesgarda.ProjectBloodMoon.game.Game.game;
-
 public class Main {
-    public static java.sql.Connection conn = null;
+    public static Connection conn;
     public static Logger logger = Logger.getLogger(Main.class.getName());
     public static BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
     public static double version = 0;
@@ -44,45 +44,41 @@ public class Main {
             ResultSet rs = stmt.executeQuery();
             rs.last();
             if(!(version >= Double.parseDouble(rs.getString("version")))) {
+                System.out.println("You are using an outdated version of the game. Downloading new version...");
                 Statement stmt2 = conn.createStatement();
                 String query2 = "SELECT link FROM pbm.versions WHERE version = \"" + rs.getString("version") + "\"";
                 ResultSet rs2 = stmt2.executeQuery(query2);
                 rs2.next();
-                System.out.println("You are using an outdated version of the game. Please download the newest version here: " + rs2.getString("link") + "\n\n(Press [ENTER] to continue)");
+                System.out.println("Downloading...");
+                System.out.println("DO NOT EXIT THE LAUNCHER!");
+
+                File oldJar = new File("ProjectBloodMoon.jar");
+                oldJar.delete();
+                URL website = new URL(rs2.getString("link"));
+                ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+                FileOutputStream fos = new FileOutputStream("ProjectBloodMoon.jar");
+                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+
+                gameProperties.setProperty("version", rs.getString("version"));
+                gameProperties.save(new ObjectOutputStream(new FileOutputStream("game.properties")), "");
+
+                System.out.println("Update finished. Please relaunch the game.\n(Press [ENTER] to exit)");
                 consoleInput.readLine();
-                exit();
+                System.exit(0);
             }
 
-            Timer timer = new Timer();
-            TimerTask timerTask = new TimerTask() {
-                @Override
-                public void run() {
-                    try {
-                        Statement stmt = conn.createStatement();
-                        String query = "SELECT * FROM pbm.accounts";
-                        stmt.executeQuery(query);
-                    }
-                    catch(Exception e1) {
-                        try {
-                            conn = DriverManager.getConnection("jdbc:mysql://98.164.253.104:3306/pbm?user=connector&password=dpass");
-                        }
-                        catch(Exception e2) {
-                            System.out.println("You are disconnected from the internet! Please reconnect if you don't want to lose progress!");
-                        }
-                    }
-                }
-            };
-            timer.scheduleAtFixedRate(timerTask, 10000, 10000);
-            game();
+            System.out.println("Launching game...");
+            String currentPath = new java.io.File(".").getCanonicalPath();
+            ProcessBuilder pb = new ProcessBuilder(System.getProperty("java.home"), "-jar", "ProjectBloodMoon.jar");
+            pb.directory(new File(currentPath));
+            Process p = pb.start();
         }
         catch(FileNotFoundException e) {
             System.out.println("Required files are missing. Cannot start game.");
         }
-        catch(SQLException e) {
-            System.out.println("Failed to connect to server. Check your connection and try relaunching the game.");
-        }
         catch(Exception e) {
-            System.out.println("Something went wrong... :(");
+            e.printStackTrace();
+            System.out.println("Failed to connect to server. Check your connection and try relaunching the game.");
         }
     }
 
--- builds/temp/direct.bat
+++ builds/temp/direct.bat
@@ -1,7 +1,6 @@
 @ECHO OFF
-CALL java -jar Launcher.jar
+CALL java -jar ProjectBloodMoon.jar
 IF "%errorlevel%" NEQ "0" (
 	echo Components to run the game are missing. Please download them at https://drive.google.com/drive/folders/1_aDBmAlZtB544GnEiuPZ-ENGvlCl6Lm8?usp=sharing
-	PAUSE
 )
 PAUSE
