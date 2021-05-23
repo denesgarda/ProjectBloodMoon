5e61fe471fb216e0b2bb5f49acdf61893cfecfc2
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-05-23 11:09:38 -0700

    Update start_on_linux.sh

--- builds/temp/start_on_linux.sh
+++ builds/temp/start_on_linux.sh
@@ -8,3 +8,5 @@ then
 elif [[ $?  -ne 0]]
 then
     echo Components to run the game are missing. Please download them at https://drive.google.com/drive/folders/1_aDBmAlZtB544GnEiuPZ-ENGvlCl6Lm8?usp=sharing
+
+sleep 5
