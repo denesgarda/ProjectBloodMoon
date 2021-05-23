c8ca5e5c32218833a5c8ffe642ed52c7ec8ca1e6
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-05-23 11:16:22 -0700

    1

--- builds/temp/start_on_linux.sh
+++ builds/temp/start_on_linux.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
+set +v
+
 cd "${0%/*}"
 java -jar Launcher.jar
-if [[ $? -eq 3000 ]]
-then
-    java -jar ProjectBloodMoon.jar
-elif [[ $?  -ne 0]]
-then
-    echo Components to run the game are missing. Please download them at https://drive.google.com/drive/folders/1_aDBmAlZtB544GnEiuPZ-ENGvlCl6Lm8?usp=sharing
 
-sleep 5
+if [ $? -eq 184 ]
+then
+    clear
+    java -jar ProjectBloodMoon.jar
+else
+    echo Components to run the game are missing. Please download them at https://drive.google.com/drive/folders/1_aDBmAlZtB544GnEiuPZ-ENGvlCl6Lm8?usp=sharing
+fi
