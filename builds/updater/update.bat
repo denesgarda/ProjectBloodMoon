27ad23e99b44514c246ec626b1aafd5079ab1d91
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-05-24 13:54:50 -0700

    u

---
+++ builds/updater/update.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+CALL java -jar Updater.jar
+PAUSE
---
+++ projects/Updater/src/main/java/com/denesgarda/ProjectBloodMoon/main.java
@@ -0,0 +1,7 @@
+package com.denesgarda.ProjectBloodMoon;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("IN UPDATER");
+    }
+}
