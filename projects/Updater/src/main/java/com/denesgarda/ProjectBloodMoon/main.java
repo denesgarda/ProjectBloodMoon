6f4bc588ecb6460fa5888caf9bc2130ee7f02e1f
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-05-24 14:10:14 -0700

    u

--- projects/Updater/src/main/java/com/denesgarda/ProjectBloodMoon/main.java
+++ projects/Updater/src/main/java/com/denesgarda/ProjectBloodMoon/main.java
@@ -9,7 +9,7 @@ public class main {
         File homeDirectory = new File(new File(".").getAbsolutePath().substring(0, new File(".").getAbsolutePath().length() - 2));
         for(String item : homeDirectory.list()) {
             File file = new File(item);
-            if(!(file.getName().equals("Updater.jar") || file.getName().equals("update.bat") || file.getName().equals("update.command") || file.getName().equals("update.sh"))) {
+            if(!(file.getName().equals("Updater.jar") || file.getName().equals("update.bat") || file.getName().equals("update.command") || file.getName().equals("update.sh") || file.getName().equals("package.zip"))) {
                 file.delete();
             }
         }
